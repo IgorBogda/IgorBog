f771031ccac6cc530fcfdeb10b5b0f80fbb043b9
IgorBogda <igoresek@gmail.com>
2024-10-25 12:10:11 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,107 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 80,
+      "initialViewParameters": {
+        "yaw": 0.1490140458862168,
+        "pitch": 0,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.249542843311362,
+          "pitch": 0.22858994367241436,
+          "rotation": 0,
+          "target": "1-1"
+        },
+        {
+          "yaw": 3.0763670763043613,
+          "pitch": 0.25188884074025886,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 80,
+      "initialViewParameters": {
+        "yaw": 1.6424047948361205,
+        "pitch": 0.12930583466709855,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6447252519653706,
+          "pitch": 0.39179523084675694,
+          "rotation": 0,
+          "target": "0-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 80,
+      "initialViewParameters": {
+        "yaw": 1.90388865039664,
+        "pitch": 0.2171344985341399,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.90388865039664,
+          "pitch": 0.2171344985341399,
+          "rotation": 0,
+          "target": "0-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
